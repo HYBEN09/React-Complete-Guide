ab658e5bdd031979726416df134053effb2d6494
hyebeen byun <hyebeen2658@naver.com>
2022-11-21 10:53:39 +0900

    조건 명령문 반환 추가하기

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import Card from "../UI/Card/Card";
 import ExpensesFilter from "./ExpensesFilter";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import { useState } from "react";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
   //select 기본값 2020으로 설정
@@ -20,20 +20,6 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  //item이 없을때 content 표시
-  let expensesContent = <p> No expenses Found </p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   //*---------------------------------------------------------------------------------
 
   return (
@@ -43,20 +29,7 @@ function Expenses(props) {
         onChangeFilter={filterChangeHandler}
       />
 
-      {/* 동적으로 아이템 추가 */}
-
-      {/*filteredExpenses.length === 0
-        ? expensesEmptyContent
-        : filteredExpenses.map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-        ))*/}
-
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 }
