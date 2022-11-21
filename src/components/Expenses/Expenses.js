d46f9a08cd3d56ebe6f97603310916c44d64ee3a
hyebeen byun <hyebeen2658@naver.com>
2022-11-21 10:10:47 +0900

    filter method이용하여 list 다루기

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,13 +12,20 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   };
 
+  //년도 에 맞는 아이템 찿아내기
+  // getFullYear() 메서드는 주어진 날짜의 현지 시간 기준 연도를 반환합니다.
+  const filterExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((expense) => (
+      {/* 동적으로 아이템 추가 */}
+      {filterExpenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
