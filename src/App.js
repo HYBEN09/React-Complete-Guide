a6f8895c8cd4e4bfeb724d1f3ea83958be7ebe36
hyebeen byun <hyebeen2658@naver.com>
2022-11-21 18:48:19 +0900

    state를 통해 사용자 목록 관리하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UserList from "./components/Users/UserList";
 
 function App() {
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    //이전 상태에 의존해서 상태를 업데이트 할때 setUserList의 대체 폼 사용 -> 함수를 전달하는 폼
+    setUserList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: userName, age: userAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
-      <UserList users={[]} />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={userList} />
     </div>
   );
 }
