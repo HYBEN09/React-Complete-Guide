ff6ee0d85072492b9846cf4f6946fe4b2aea3380
hyebeen byun <hyebeen2658@naver.com>
2022-11-22 12:53:44 +0900

    useEffect 사용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -7,13 +7,34 @@ import MainHeader from "./components/MainHeader/MainHeader";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  //* --------------------------------------------------------------------------
+
+  //컴토넌트 함수에 직접 실행하고 싶지 않아서 useEffect()안에서 실행
+  //실제로 앱이 시작될 때 한 번만 실행됩니다 그 이후로 의존성은 절대 변경되지 않기 때문입니다
+  //여기에는 의존성([dependencies])이 없기 때문입니다.
+  //이 코드를 한 번 실행하고 싶습니다 그리고 그건 앱이 시작되는 경우죠
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === 1) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  //* --------------------------------------------------------------------------
+
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+    //사용자가 로그인 했으면 = 1, 아니면 = 0
+    //무언가를 저장 하고 싶을 때 사용.
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    //로그아웃 버튼을 클릭할 때마다 다시 로컬 저장소에 연결하도록 합니다 거기에서 isLoggedIn 키를 제거합니다
+    localStorage.removeItem("isLoggedIn");
+
     setIsLoggedIn(false);
   };
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,34 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
   const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  //* --------------------------------------------------------------------------
+
+  //? enteredEmail 또는 enteredPassword가 바뀔 때마다 다시 실행.
+  //이메일 또는 비밀번호 필드의 키 입력에 대한 응답으로 해당 폼의 유효성을 확인하고 업데이트하는 것
+  //또한 사이드이펙트라고 할 수 있습니다
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log("Checking form validity!");
+      setFormIsValid(
+        enteredEmail.trim().length > 6 && enteredEmail.includes("@")
+      );
+    }, 500);
+
+    //clean up function
+    //모든 새로운 사이드이펙트 함수가 실행되기 전에, 그리고 컴포넌트가 제거되기 전에 실행됩니다
+    //첫 번째 사이드이펙트 함수가 실행되기 전에는 실행되지 않습니다. 그러나 그 이후에는 실행됩니다
+    return () => {
+      console.log("cleanup");
+      clearTimeout(identifier);
+    };
+  }, [enteredEmail, enteredPassword]);
+
+  //* --------------------------------------------------------------------------
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    setEmailIsValid(enteredEmail.includes("@"));
   };
 
   const validatePasswordHandler = () => {
@@ -45,7 +62,7 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
@@ -59,7 +76,7 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
